6b3b883e55516ad005f1ac624627ab8db2ec30c0
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2020-03-11 01:14:52 -0500

    Created prefJS

---
+++ packages/prefjs/src/prefjs.ts
@@ -0,0 +1,103 @@
+import { get } from 'dot-prop';
+
+export default class PrefJS {
+  private defaultLocale = 'en';
+  private currentLocale: string = this.defaultLocale;
+  private data = {};
+
+  constructor(params?: { defaultLocale: string, currentLocale?: string }) {
+    if (params?.defaultLocale) {
+      this.defaultLocale = params.defaultLocale;
+
+      if (params.currentLocale) {
+        this.currentLocale = params.currentLocale;
+      }
+    }
+  }
+
+  /**
+   * Sets the default locale
+   * @param locale
+   * @example
+   * setDefaultLocale('es');
+   */
+  setDefaultLocale(locale: string): PrefJS {
+    this.defaultLocale = locale;
+    return this;
+  }
+
+  /**
+   * Set the current locale
+   * @param locale
+   */
+  setCurrentLocale(locale: string): PrefJS {
+    this.currentLocale = locale;
+    return this;
+  }
+
+  /**
+   * Sets the data for the entire configuration
+   * @param data Example: { 'en': { ...an object with properties in english }, 'es': { ...the same object with properties in spanish }}
+   * @example
+   * setGlobalData({
+   *  en: { title: 'My home', description: 'It is my home.' },
+   *  es: { title: 'Mi casa', description: 'Es mi casa.' },
+   *  fr: { title: 'Ma maison', description: 'C\'est ma maison' }
+   * });
+   */
+  setGlobalData(data: { [key:string]: any }): PrefJS {
+    this.data = data;
+    return this;
+  }
+
+  /**
+   * Sets the data for the given locale
+   * @param locale
+   * @param data
+   * @example
+   * setData('es', {
+   *  pageTitle: 'Érase una vez en Hollywood',
+   *  director: 'Quentin Tarantino'
+   * });
+   */
+  setLocaleData(locale: string, data: any): PrefJS {
+    this.data[locale] = data;
+    return this;
+  }
+
+  /**
+   * Gets all the data stored for a single locale
+   * @param locale
+   */
+  getLocaleData(locale: string): any {
+    return this.data[locale];
+  }
+
+  /**
+   * Returns all the locales configured
+   * @example
+   * getLocalesConfigured(); // ['en', 'es', 'pt']
+   */
+  getLocalesConfigured(): string[] {
+    return Object.keys(this.data);
+  }
+
+  /**
+   * Gets a value from the loaded data, falling back to the
+   * value from the locale and or to the fallbackValue given
+   * @param path
+   * @param fallbackValue
+   */
+  get(path: string, fallbackValue?: string) {
+    // Get from the desired locale first
+    let value: any = get(this.data[this.currentLocale], path);
+
+    // If the value is undefined, try to get the value
+    // from the default locale
+    if (typeof value === 'undefined') {
+      value = get(this.data[this.defaultLocale], path, fallbackValue);
+    }
+
+    return value;
+  }
+}
